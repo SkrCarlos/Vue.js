f37ca779cd88fd068378383a71acba14ae995c86
Carlos Carreon <skrcarlos_carreon@outlook.com>
2022-01-26 15:54:43 -0600

    Subiendo todas las carpetas seccion 1

---
+++ Seccion 1/Ganchos de Vida/vue.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+    el: '#vm',
+    data: {
+        mensaje: 'Este es el mensaje'
+    }, 
+    beforeCreate : function () {
+        console.log('Llamando la funcion beforeCreate');
+    }, 
+    created : function () {
+        console.log('Llamando created');
+    },
+    beforeMount : function () {
+        console.log('Llamando beforeMount');
+    }, 
+    mounted : function () {
+        console.log('Llamando mounted');
+    }, 
+    created : function () {
+        console.log('Llamando created');
+    }, 
+    beforeUpdate : function () {
+        console.log('Llamando beforeUpdate');
+    }, 
+    updated : function () {
+        console.log('Llamando updated');
+    }, 
+    beforeDestroy : function () {
+        console.log('Llamando beforeDestroy');
+    }, 
+    destroyed : function () {
+        console.log('Llamando destroyed');
+    },
+    methods: {
+        destruir: function () {
+            this.$destroy();
+        }
+    }
+});
